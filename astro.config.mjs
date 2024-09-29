4c7ff04c0df4a19e7b736af7592ca9886ec5266c
Serhii <s.shramko@macpaw.com>
2024-09-28 20:26:52 -0400

    feat: add sitemap & robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 import { defineConfig, sharpImageService } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://links.shramko.dev/',
   output: 'server',
+
   image: {
     service: sharpImageService(),
   },
+
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     }
   }),
+
+  integrations: [sitemap()],
 });
