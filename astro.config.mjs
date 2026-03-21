aa6ec4eb384007efe924ce55265919e9ae135d0f
shramko <serhii.shramko@betterme.world>
2026-03-21 17:08:55 -0400

    chore: move website to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig, sharpImageService } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://links.shramko.dev/',
-  output: 'server',
 
   image: {
     service: sharpImageService(),
